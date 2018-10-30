24c8a16f582105bf832ddb89d0ddc465c586cea3
PE Hladik <pehladik@gmail.com>
2018-10-30 16:26:55 +0100

    correction erreur demarrage CAN nucleo

--- nucleo/Src/main.c
+++ nucleo/Src/main.c
@@ -323,9 +323,9 @@ void _Error_Handler(char *file, int line)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
-  while(1) 
+  /*while(1)
   {
-  }
+  }*/
   /* USER CODE END Error_Handler_Debug */
 }
 
