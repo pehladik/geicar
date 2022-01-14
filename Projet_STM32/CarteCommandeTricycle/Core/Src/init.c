d8bce057fdca07c2f1606a3384d88f3d194063da
Yohan Simard <ysimard@etud.insa-toulouse.fr>
2022-01-14 17:33:27 +0100

    [stm32] send ultrasonic sensor measures periodically through CAN

--- Projet_STM32/CarteCommandeTricycle/Core/Src/init.c
+++ Projet_STM32/CarteCommandeTricycle/Core/Src/init.c
@@ -22,12 +22,16 @@ _Noreturn void Main(void) {
 	init();
 	CAN_start(&hcan1);
 
-	uint8_t data[] = {1};
+	uint8_t data[8] = {1};
 	CAN_send(&hcan1, 0xf1, data, 1);
 
 	while (1) {
 		double distance = ultrasound_get_distance_trig_input();
 		uart_print("Distance = %.1f cm\r\n", distance);
+		uint16_t distance_converted = (uint16_t)(distance * 10);
+		data[0] = (distance_converted >> 8) & 0xff;
+		data[1] = distance_converted & 0xff;
+		CAN_send(&hcan1, 0xf4, data, 2);
 		HAL_Delay(200);
 	}
 }
